3ed949346fab16a7ef41801909f51388a4e83720
YuviShendge <yuvi.shen.dge@gmail.com>
2026-03-02 14:01:53 -0500

    Added and tested the LRU policy

--- src/CacheEviction.py
+++ src/CacheEviction.py
@@ -19,6 +19,31 @@ def fifo(k, requests):
 
     return misses
 
+def lru_policy(k, requests):
+    cache = []
+    last_used = {}
+    misses = 0
+
+    for time in range(len(requests)):
+        r = requests[time]
+        if r in cache:
+            last_used[r] = time
+        else:
+            misses += 1
+
+            if len(cache) < k:
+                cache.append(r)
+                last_used[r] = time
+            else:
+                # to remove the most recent access time is the oldest
+                lru_item = min(cache, key=lambda x: last_used[x])
+                cache.remove(lru_item)
+
+                cache.append(r)
+                last_used[r] = time
+
+    return misses
+
 def main():
     if len(sys.argv) != 2:
         print("Usage: python cache_sim.py <input_file>")
@@ -28,13 +53,17 @@ def main():
 
     with open(filename, 'r') as f:
         first_line = f.readline().strip().split()
-        k = int(first_line[0])
+        k = int(first_line[0]) # capacity
+        m = int(first_line[1]) # seq of requests
 
         requests = list(map(int, f.readline().strip().split()))
 
     fifo_misses = fifo(k, requests)
+    lru_misses = lru_policy(k, requests)
 
     print("FIFO  :", fifo_misses)
+    print("LRU  :", lru_misses)
+
 
 
 if __name__ == "__main__":
